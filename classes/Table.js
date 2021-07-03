7e8adcf8fca0f61db775f777459705a9dc1d7ffa
Donn <donnpie@gmail.com>
2021-07-03 12:15:14 +0200

    First working version

--- classes/Table.js
+++ classes/Table.js
@@ -15,4 +15,25 @@ class Table {
             console.log(row);
         }
     }
+
+    rowsToHtml(settings) {
+        //Prints rows to an html string
+        //It is assumed that the string will be inserted as html inbetween <tbody> tags
+        let rows = '';
+        for (let row of this.rows) {
+            let rowHtml = 
+                `<tr>
+                    <td>${row.period}</td>
+                    <td>${row.age}</td>
+                    <td>${Math.round(row.openingBalance)}</td>
+                    <td>${Math.round(row.contributions)}</td>
+                    <td>${Math.round(row.getInterestOnContributions(settings.growthRate))}</td>
+                    <td>${Math.round(row.getInterestOnOpeningBalance(settings.growthRate))}</td>
+                    <td>${Math.round(row.getClosingBalance(settings.growthRate))}</td>
+                </tr>`;
+            //rows.append(rowHtml);
+            rows += rowHtml;
+        }
+        return rows;
+    }
 }
--- classes/logic.js
+++ classes/logic.js
@@ -1,20 +1,106 @@
-let settings = new Settings(30, 0.02, 0.08);
-//console.log(settings.numOfPeriods);
-let tableRow1 = new TableRow(0, 2021, 42, 400000, 8000);
-//tableRow1.printRow();
-//let tableRow2 = new TableRow(1, 2022, 43, 440320, 8160);
-let rows = [];
-rows.push(tableRow1);
-for (let i = 1; i <= settings.numOfPeriods; i++) {
-    const period = rows[i-1].period + 1;
-    const year = rows[i-1].year + 1;
-    const age = rows[i-1].age + 1;
-    const ob = rows[i-1].getClosingBalance(settings.growthRate);
-    const contributions = rows[i-1].contributions * (1+ settings.contributionIncreaseRate);
-    let newRow = new TableRow(period, year, age, ob, contributions);
-    rows.push(newRow);
-    //table.addRow(newRow);
-}
-table = new Table(rows);
 
-table.printRows();
+
+const startYear = document.getElementById('year');
+const startAge = document.getElementById('age');
+const openingBalance = document.getElementById('opening-balance');
+const startcontributions = document.getElementById('contributions');
+const numberOfPeriods = document.getElementById('number-of-periods');
+const contributionIncrease = document.getElementById('contribution-increase');
+const growthRate = document.getElementById('growth-rate');
+
+const errorElem = document.getElementById('error')
+
+const form = document.getElementById('input-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    errorMessages = [];
+    //Toggle input box colors when there is an error
+    validate(startYear, 'Start year is required', errorMessages)
+    validate(startAge, 'Start age is required', errorMessages)
+    validate(openingBalance, 'Opening balance is required', errorMessages)
+    validate(startcontributions, 'Start contributions is required', errorMessages)
+    validate(numberOfPeriods, 'Number of periods is required', errorMessages)
+    validate(contributionIncrease, 'Contribution increase is required', errorMessages)
+    validate(growthRate, 'Growth rate is required', errorMessages)
+    //console.log(typeof(startYear.value));
+    
+    
+    //Print error messages to screen
+    if (errorMessages.length > 0) {
+        //alert(errorMessages); 
+        errorElem.innerText = errorMessages.join(', ');
+        return;
+    }
+    //console.log (errorMessages.length);
+    //console.log(typeof(parseInt(startYear.value)));
+    console.log(typeof(parseInt(numberOfPeriods.value)));
+
+    //Define the settings
+    let settings = new Settings(
+        getNumber(numberOfPeriods, 'Int'), 
+        getNumber(contributionIncrease, 'Float'),
+        getNumber(growthRate, 'Float'),
+    );
+    //console.log(settings);
+    
+
+    //Define row for starting period
+    let startRow = new TableRow(
+        0, 
+        getNumber(startYear, 'Int'), 
+        getNumber(startAge, 'Int'), 
+        getNumber(openingBalance, 'Int'), 
+        getNumber(startcontributions, 'Int'), 
+    );
+    //console.log(startRow);
+
+    //Add the rest of the rows
+    let rows = [];
+    rows.push(startRow);
+    for (let i = 1; i <= settings.numOfPeriods; i++) {
+        const period = rows[i-1].period + 1;
+        const year = rows[i-1].year + 1;
+        const age = rows[i-1].age + 1;
+        const ob = rows[i-1].getClosingBalance(settings.growthRate);
+        const contributions = rows[i-1].contributions * (1+ settings.contributionIncreaseRate);
+        let newRow = new TableRow(period, year, age, ob, contributions);
+        rows.push(newRow);
+    }
+    let table = new Table(rows);
+
+    //table.printRows();
+
+    //Create dynamic HTML table
+    tableBody = document.getElementById('table-body');
+    let rowsHtml = table.rowsToHtml(settings);
+    //console.log(rowsHtml);
+    tableBody.innerHTML = rowsHtml;
+
+
+
+});
+
+function validate(field, errorMessage, errorMessages) {
+    if (field.value === '' || field.value === null) {
+        errorMessages.push(errorMessage);
+    }
+}
+
+function getNumber(elem, numberType) {
+    if (numberType === "Int")
+        return parseInt(elem.value);
+        if (numberType === "Float")
+        return parseFloat(elem.value);
+}
+
+
+//References
+//https://www.youtube.com/watch?v=In0nB0ABaUk
+//https://stackoverflow.com/questions/34057595/allow-2-decimal-places-in-input-type-number/34057860
+//https://gomakethings.com/converting-strings-to-numbers-with-vanilla-javascript/
+//https://www.w3schools.com/tags/att_input_value.asp
+//https://youtu.be/ri5Nqe_IK50
+//https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+//https://stackoverflow.com/questions/12114570/how-to-align-texts-inside-of-an-input
+//https://pawelgrzybek.com/rounding-and-truncating-numbers-in-javascript/
